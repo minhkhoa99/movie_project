bd62495b81f336e6e98c9ec426946f586231eeca
minhkhoa99 <110026964+minhkhoa99@users.noreply.github.com>
2023-02-23 00:13:46 +0700

    commit db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 // import { UserController } from './user/user.controller';
 import { UserModule } from './user/user.module';
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-    imports:[UserModule],
+    imports:[ TypeOrmModule.forRoot({
+        type: 'mysql',
+        host: 'localhost',
+        port: 3306,
+        username: 'root',
+        password: '123456a@',
+        database: 'tbl_movies',
+        entities: [],
+        synchronize: true,})],
     
 })
 export class AppModule{
